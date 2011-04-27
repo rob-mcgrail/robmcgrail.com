53ea9a900cbc239d5e7ca0346bb995a3c62d173e
rob <rob@aichi.(none)>
2011-04-27 23:11:25 +1200

    made abstract cache, added in old cachetools module, made new hardcache

--- config/constants.rb
+++ config/constants.rb
@@ -16,7 +16,7 @@ SETTINGS[:app_layout] = 'layouts/application'
 # GA tracking code, used in the ga_tracking template helper
 SETTINGS[:ga_caode] = 'UA-10148230-1'
 
-# While true, throws proper errors. When false, errors run the Error#bug controller
+# While true, throws proper errors. When nil, errors run the Error#bug controller
 SETTINGS[:development_mode] = true
 
 # Chose coderay stylesheet (public/stylesheets/code/) - rack, idlecopy,
@@ -24,6 +24,9 @@ SETTINGS[:coderay_css] = 'rack'
 
 # Maximum bytes for the template chache (caches raw haml in memory to save on disk reads)
 SETTINGS[:template_cache_max] = 1_000_000
+
+# Maximum bytes for the hard chache (caches compiled html in memory to save on haml/db cycles)
+SETTINGS[:hard_cache_max] = 1_000_000
 
 # Location of views. Slash on both sides, in app root.
 SETTINGS[:view_folder] = '/views/'
