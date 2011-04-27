134680b653b4ee7d5b02a52ec4fb819ba893bf7e
rob <rob@aichi.(none)>
2011-04-27 23:11:42 +1200

    added clear all cache controller

--- controllers/adminful_controller.rb
+++ controllers/adminful_controller.rb
@@ -7,5 +7,15 @@ class Adminful < AbstractController
     TemplateCache.clear
     render 'errors/generic'
   end
+
+  R.add '/render_again', 'Adminful#clear_all_caches', 'clear_caches'
+  # Clears the template chache
+  def refresh_templates
+    @title = 'Cleared all caches'
+    @error = 'Cleared all caches'
+    TemplateCache.clear
+    HardCache.clear
+    render 'errors/generic'
+  end
 end
 
