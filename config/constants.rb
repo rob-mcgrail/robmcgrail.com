ab38ace6ba24c25fef22c3de3d393635404a2c55
rob <rob@aichi.(none)>
2011-05-21 22:34:34 +1200

    rearranged constants

--- config/constants.rb
+++ config/constants.rb
@@ -1,8 +1,5 @@
-# Where the public folders live
-PUBLIC_ROOT = 'public'
-
-# The public folders - best not to change these as the template helpers assume they exist as below
-PUBLIC_FOLDERS = ['/audio', '/files', '/images', '/javascript', '/stylesheets', '/video', '/tmp', '/robots.txt']
+# While true, throws proper errors. When nil, errors run the Error#bug controller
+SETTINGS[:development_mode] = true
 
 # The name of the site - used in title template helper
 SETTINGS[:sitename] = 'robmcgrail.com'
@@ -10,14 +7,26 @@ SETTINGS[:sitename] = 'robmcgrail.com'
 # url of the site
 SETTINGS[:hostname] = '0.0.0.0'
 
-# The base template - this should lay out everything up to and including <body>, then yield
-SETTINGS[:app_layout] = 'layouts/application'
-
 # GA tracking code, used in the ga_tracking template helper
 SETTINGS[:ga_caode] = 'UA-10148230-1'
 
-# While true, throws proper errors. When nil, errors run the Error#bug controller
-SETTINGS[:development_mode] = true
+# Secret for salting things.
+SETTINGS[:secret] = '73rd7yr879rweri34r8y7wrwery8i7dfgwr87tkjgjhfjhfjhgjhgjftrd65e76r7t87t8'
+
+# Database connection
+SETTINGS[:db] = 'sqlite://' + PATH + '/db/main'
+
+# The base template - this should lay out everything up to and including <body>, then yield
+SETTINGS[:app_layout] = 'layouts/application'
+
+# Where the public folders live
+SETTINGS[:public_root] = 'public'
+
+# The public folders - best not to change these as the template helpers assume they exist as below
+SETTINGS[:public_folders] = ['/audio', '/files', '/images', '/javascript', '/stylesheets', '/video', '/tmp', '/robots.txt']
+
+# Location of views. Slash on both sides, in app root.
+SETTINGS[:view_folder] = '/views/'
 
 # Chose coderay stylesheet (public/stylesheets/code/) - rack, idlecopy,
 SETTINGS[:coderay_css] = 'rack'
@@ -27,10 +36,4 @@ SETTINGS[:template_cache_max] = 1_000_000
 
 # Maximum bytes for the hard chache (caches compiled html in memory to save on haml/db cycles)
 SETTINGS[:hard_cache_max] = 1_000_000
-
-# Location of views. Slash on both sides, in app root.
-SETTINGS[:view_folder] = '/views/'
-
-# Secret for salting things.
-SETTINGS[:secret] = '73rd7yr879rweri34r8y7wrwery8i7dfgwr87tkjgjhfjhfjhgjhgjftrd65e76r7t87t8'
 
