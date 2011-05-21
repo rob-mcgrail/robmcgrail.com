1dec664b2f06eed57ee86f4f5e763445fc879f8a
rob <rob@aichi.(none)>
2011-05-21 22:34:58 +1200

    addded insane, just really fucking ridiculous, controllers for the db

--- controllers/adminful_controller.rb
+++ controllers/adminful_controller.rb
@@ -8,6 +8,7 @@ class Adminful < AbstractController
     render 'errors/generic'
   end
 
+
   R.add '/render_again', 'Adminful#clear_all_caches', :name => 'clear_caches'
   # Clears the template chache
   def clear_all_caches
@@ -15,6 +16,31 @@ class Adminful < AbstractController
     @error = 'Cleared all caches'
     TemplateCache.clear
     HardCache.clear
+    render 'errors/generic'
+  end
+
+
+  R.add '/destructively_update_db', 'Adminful#destructively_update_db'
+  # Wipes and updates the database
+  # Only available in development mode for now
+  def destructively_update_db
+    @title = 'Destructively update database'
+    if SETTINGS[:development_mode]
+      DataMapper.auto_migrate!
+      @error = 'Updated database'
+    else
+      @error = 'Can\'t destructively upate database on a production install'
+    end
+    render 'errors/generic'
+  end
+
+
+  R.add '/update_db', 'Adminful#update_db'
+  # Only available in development mode for now
+  def update_db
+    @title = 'Update database'
+    @error = 'Updated database'
+    DataMapper.auto_upgrade!
     render 'errors/generic'
   end
 end
