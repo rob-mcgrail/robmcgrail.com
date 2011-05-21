2c19acf0634f36ba7ef767e2a432b2d97a78a9e9
rob <rob@aichi.(none)>
2011-05-21 18:55:46 +1200

    splitting file includes in to user and core

---
+++ core.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+
+# gem dependencies
+require 'RedCloth'
+require 'haml'
+require 'coderay'
+require 'haml-coderay'
+
+# standlib requirements
+require 'ostruct'
+
+PATH = File.expand_path(File.dirname(__FILE__))
+
+SETTINGS = {}
+
+$:.unshift PATH
+
+require 'config/constants'
+require 'config/gem_settings'
+
+require 'core/template_helpers/includes'
+require 'core/template_helpers/core'
+
+require 'core/caches/abstract_cache'
+require 'core/caches/template_cache'
+require 'core/caches/hard_cache'
+
+require 'core/renderer'
+require 'core/router'
+require 'core/dispatcher'
+require 'core/abstract_controller'
+
