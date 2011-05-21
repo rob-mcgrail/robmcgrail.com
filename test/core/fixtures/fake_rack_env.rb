212d92128a6faabaff776823697efcd0969dc9be
rob <rob@aichi.(none)>
2011-05-21 18:56:15 +1200

    replace this some day - updates for post? and get? for my fake request

--- test/core/fixtures/fake_rack_env.rb
+++ test/core/fixtures/fake_rack_env.rb
@@ -3,12 +3,21 @@ require 'rack'
 class Env < Rack::Request
   attr_accessor :path
 
-  def initialize(path)
+  def initialize(path, type = 'get')
     @path = path
+    self.instance_variable_set(('@' + type).to_sym, true)
   end
 
   def POST()
     {}
+  end
+
+  def get?
+    @get
+  end
+
+  def post?
+    @post
   end
 end
 
