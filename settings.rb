d2534b32684b27d2cab80c55669f74cc164cbd45
rob <rob@aichi.(none)>
2011-06-07 20:33:39 +1200

    added production database

--- settings.rb
+++ settings.rb
@@ -1,19 +1,19 @@
 configure :development do
   set :db, 'sqlite3://' + settings.root + '/db/development.sqlite3'
+	set :raise_errors, true
   set :show_exceptions, true
   set :static, false
   set :sessions, true
   set :highlighting, 'zenburn'
-	set :raise_errors, true
 end
 
 configure :production do
   set :db, 'sqlite3://' + settings.root + '/db/production.sqlite3'
+	set :raise_errors, false
   set :show_exceptions, false
   set :static, false
   set :sessions, true
   set :highlighting, 'rack'
-	set :raise_errors, false
 end
 
 # Database
