8d05984f26c94440ae379cc91c701431f02e4e13
rob <rob@aichi.(none)>
2011-09-24 15:41:39 +1200

    tidied includes

--- app.rb
+++ app.rb
@@ -1,21 +1,32 @@
 require 'rubygems'
+
+# Sinatra
 require 'sinatra'
 require 'sinatra/url_for'
 require 'sinatra/static_assets'
 require 'sinatra/flash'
+
+# Models
 require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
 require 'dm-migrations'
 require 'dm-tags'
+
+# Authentication
 require 'warden'
 require 'bcrypt'
+
+# Templates
 require 'RedCloth'
 require 'haml'
 require 'coderay'
 require 'haml-coderay'
 require 'redclothcoderay'
+
+# XML/Solr
 require 'nokogiri'
+require 'rest_client'
 
 require 'settings'
 
