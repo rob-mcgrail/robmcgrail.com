77c0263b19246b729a48cc3c1697c7c584808b0a
Rob McGrail <rob@aichi.(none)>
2011-12-15 00:22:04 +1300

    tidied settings

--- settings.rb
+++ settings.rb
@@ -1,14 +1,24 @@
-configure :development do
-  set :db, 'sqlite3://' + settings.root + '/db/development.sqlite3'
-	set :raise_errors, true
-  set :show_exceptions, true
-  set :static, false
+configure do
   set :sessions, true
   set :highlighting, 'rack'
   set :logging, false # stop annoying double log messages...
   set :session_secret, "Ns7uwEYgHlvYp9-!.:R%*s{=#4}05J|'!*?YS[Ah7dn_SWa'.?(yMJ&.EzOskg"
   set :background, 'http://catlovers.todayblogpost.com/wp-content/uploads/2011/01/wpid-ScottishFoldHistory21.jpg'
   set :method_override, true
+  set :reddit_feed, 'http://www.reddit.com/user/spidermonk/comments.rss'
+  set :lastfm_feed, 'http://ws.audioscrobbler.com/1.0/user/robomc/recenttracks.rss'
+  set :twitter_feed, 'http://twitter.com/statuses/user_timeline/20491432.rss'
+  set :github_feed, 'https://github.com/robomc.atom'
+end
+
+configure :development do
+  set :db, 'sqlite3://' + settings.root + '/db/development.sqlite3'
+	set :raise_errors, true
+  set :show_exceptions, true
+  set :static, false
+  set :background, 'http://catlovers.todayblogpost.com/wp-content/uploads/2011/01/wpid-ScottishFoldHistory21.jpg'
+  set :method_override, true
+  set :haml, {:format => :html5, :ugly => true }
 end
 
 configure :production do
@@ -19,9 +29,7 @@ configure :production do
   set :sessions, true
   set :highlighting, 'zenburn'
   set :logging, false # stop annoying double log messages...
-  set :session_secret, "Ns7uwEYgHlvYp9-!.:R%*s{=#4}05J|'!*?YS[Ah7dn_SWa'.?(yMJ&.EzOskg"
-  set :background, 'http://catlovers.todayblogpost.com/wp-content/uploads/2011/01/wpid-ScottishFoldHistory21.jpg'
-  set :method_override, true
+  set :haml, {:format => :html5, :ugly => true }
 end
 
 # Database
@@ -30,11 +38,6 @@ DataMapper::Logger.new($stdout, :info) if settings.development?
 DataMapper.setup(:default, settings.db)
 
 # Gems
-
-HAML_OPTS = {
-  :format => :html5,
-  :ugly => true,
-}
 
 Haml::Filters::CodeRay.encoder_options = {
   :css => :class,
