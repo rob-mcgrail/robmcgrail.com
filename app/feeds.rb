1f6581bb2e338653e1dbaec4929c9030d760b4d2
Rob McGrail <rob@debian.(none)>
2011-12-16 12:06:30 +1300

    less feeds

--- app/feeds.rb
+++ app/feeds.rb
@@ -84,7 +84,7 @@ class Feeds
     a = a + self.github( (i/2).to_i ) 
     a = a + self.twitter( (i/3).to_i )
     a = a + self.reddit( (i/6).to_i )
-    a = a + self.lastfm( i )
+    a = a + self.lastfm( (i/2).to_i )
     a.sort! {|x,y| y.date <=> x.date}
   end
 
