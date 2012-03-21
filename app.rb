df602a304a5de3650cebfb1d3e9aed15bdf45fce
Rob McGrail <rob@aichi.(none)>
2012-03-21 21:09:11 +1300

    tidyup

--- app.rb
+++ app.rb
@@ -2,25 +2,14 @@
 require 'sinatra'
 require 'sinatra/static_assets'
 require 'sinatra/flash'
-# Database
-#require 'dm-core'
-#require 'dm-aggregates'
-#require 'dm-validations'
-#require 'dm-timestamps'
-#require 'dm-migrations'
-# Redis
-#require 'redis'
-# Templates
+
+# Templates/markup
 require 'haml'
 require 'RedCloth'
 require 'bluecloth'
-# Authentication
-#require 'warden'
-#require 'bcrypt'
+
 
 require 'settings'
 
 Dir['modules/*.rb'].each {|file| require file }
 Dir['app/*.rb'].each {|file| require file }
-
-#DataMapper.finalize
--- settings.rb
+++ settings.rb
@@ -5,8 +5,8 @@ error ::Exception do
 end
 
 configure do
-  set :method_override, true # For HTTP verbs
-  set :sessions, true
+  set :method_override, false # For HTTP verbs
+  set :sessions, false
   set :logging, false # stops annoying double log messages.
   set :static, false # see config.ru for dev mode static file serving
   set :feed_cache, 120
@@ -19,29 +19,14 @@ configure do
 end
 
 configure :development do
-  set :db, 'sqlite3://' + settings.root + '/db/development.sqlite3'
   set :raise_errors, true
   set :show_exceptions, true
   set :haml, {:format => :html5, :ugly => false, :escape_html => true}
 end
 
 configure :production do
-  set :db, 'sqlite3://' + settings.root + '/db/production.sqlite3'
   set :raise_errors, false
   set :show_exceptions, false
   set :haml, {:format => :html5, :ugly => true, :escape_html => true}
 end
 
-# Database
-# http://datamapper.rubyforge.org/dm-core/DataMapper.html
-
-#DataMapper.setup(:default, settings.db)
-
-#DataMapper::Property::String.length(255)
-#DataMapper::Property.required(true)
-#DataMapper::Logger.new($stdout, :info) if settings.development?
-
-# Redis
-# http://redis.io/commands
-#$redis = Redis.new
-#$redis.select settings.redis
